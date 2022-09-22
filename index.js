74128f9a61e307d2b8dca7eed093640e110d9508
VU BINH CHUONG <chuongvbgch190772@fpt.edu.vn>
2022-09-22 17:27:44 +0700

    ok

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const port = process.env.PORT || 3000;
 const web = http.createServer((req, res) => {
     res.end("This is a tutorial about NodeJS");
 });
-
-
 web.listen(port, () => {
     console.log("http://localhost:" + port);
 });
